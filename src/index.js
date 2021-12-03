dd5d62a43f9a60f747adb2e656762f185a506cb1
aithrajbouty <aithrabouty@gmail.com>
2021-12-03 08:06:56 +0700

    upa\date index.js

--- src/index.js
+++ src/index.js
@@ -28,14 +28,22 @@ app.use("/jabatan", jabatanRoute)
 app.use("/dapil", dapilRoute)
 app.use("/partai", partaiRoute)
 
-app.get('/*', (req, res) => res.send('Index Page'));
+// app.get('/*', (req, res) => res.send('Index Page'));
 
-// if (process.env.NODE_ENV === "production") {
-//    app.use(express.static("client/build"));
-//    app.get("*", function (req, res) {
-//       res.sendFile(path.join(__dirname, "./client/build/index.html"));
-//  });
-//  }
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+   app.get("*", function (req, res) {
+      res.sendFile(path.join(__dirname, "./client/build/index.html"));
+ });
+ }
+
+// if(process.env.NODE_ENV === 'production'){
+//    //set static folder
+//    app.use(express.static('client/build'));
+// }
+// app.get('*',(req, res) => {
+//    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
 app.listen(process.env.PORT || 3000, () => {
    console.log("server is running on port 3000")
